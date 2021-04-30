72458277b28a22a6873102a014fdfa19492125f7
geoffroy <geoffroy.dutot@jymeo.net>
2021-04-30 09:18:29 +0200

    Get all products - phones with cache

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use App\Repository\PhoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class ProductController extends AbstractController
 {
     /**
      * @Route("/products", name="products", methods={"GET"})
      */
-    public function getProducts(PhoneRepository $phoneRepository)
+    public function getProducts(PhoneRepository $phoneRepository, CacheInterface $cache)
     {
-        return $this->json($phoneRepository->findBy([], ['releaseDate' => 'DESC']), 200, [], ['groups' => 'get:products']);
+        return $cache->get('products', function (ItemInterface $item) use($phoneRepository) {
+            $item->expiresAfter(30);
+
+            return $this->json($phoneRepository->findBy([], ['releaseDate' => 'DESC']), 200, [], ['groups' => 'get:products']);
+        });
     }
 }
