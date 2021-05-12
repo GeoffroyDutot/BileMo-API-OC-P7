18bc8c15fe385366cbdbe6b8250811590c16a35e
geoffroy <geoffroy.dutot@jymeo.net>
2021-05-12 15:46:05 +0200

    Company Fixtures

---
+++ src/DataFixtures/CompanyFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Company;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class CompanyFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager) {
+        $company = new Company();
+        $company->setName('Amazone');
+        $company->setEmail('admin-amazone@aws.fr');
+        $password = $this->encoder->encodePassword($company, 'Amz0nePass2021!');
+        $company->setPassword($password);
+        $manager->persist($company);
+
+        $company1 = new Company();
+        $company1->setName('Fnak');
+        $company1->setEmail('contact-fnak@gmail.com');
+        $password1 = $this->encoder->encodePassword($company1, 'FnaK2020@0');
+        $company1->setPassword($password1);
+        $manager->persist($company1);
+
+        $company2 = new Company();
+        $company2->setName('TaupeAchat');
+        $company2->setEmail('taupachat@gmail.com');
+        $password2 = $this->encoder->encodePassword($company2, 'TaupeAchate2021!');
+        $company2->setPassword($password2);
+        $manager->persist($company2);
+
+        $manager->flush();
+    }
+}
